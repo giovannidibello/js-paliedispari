d157f2c30dcb2139b86e4b8fe563a5dca75b058d
Giovanni <dibellogio@gmail.com>
2025-01-08 14:20:56 +0100

    add solution ex Palindroma

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -3,9 +3,32 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // chiedere all'utente di inserire una parola
+let parolaUtente = prompt("Inserire una parola");
 
-// creare una funzione per capire se la parola inserita è palindroma 
+console.log(verificaPalindroma (parolaUtente));
 
-// fare il reverse della parola
+// FUNZIONI
 
-// confrontare la parola e quella al contrario se sono uguali è palindroma
+// creare una funzione per capire se la parola inserita è palindroma
+function verificaPalindroma (parola) {
+
+    // fare il reverse della parola
+    let splitStringa = parola.split("");
+
+    let reverseArray = splitStringa.reverse();
+
+    let joinStringa = reverseArray.join("");
+
+    // confrontare la parola e quella al contrario se sono uguali è palindroma
+
+    if (parola === joinStringa) {
+        
+        return "Palindroma"
+    } else {
+        return "Non è Palindroma"
+    }
+        
+} 
+
+
+
